9a03c0cfeb7f8b7bd36f0776deae9b4cf2f8c4e2
Michael Salaverry <michaels@axonize.com>
2019-07-30 22:11:33 +0300

    feat: should have a way of accessing its current state

--- __tests__/basics.test.ts
+++ __tests__/basics.test.ts
@@ -4,8 +4,16 @@ describe('Finite State Machine', () => {
   it('should be initializable with states', () => {
     // Arrange
     const anFSM = new FSM(['on']);
-    
+
     // Assert
     expect(anFSM).toBeDefined();
+  })
+  it('should have a current state on initialization', () => {
+    // Arrange
+    const firstState = 'on'
+    const anFSM = new FSM([firstState]);
+
+    // Assert
+    expect(anFSM.getCurrentState()).toEqual(firstState);
   })
 })
