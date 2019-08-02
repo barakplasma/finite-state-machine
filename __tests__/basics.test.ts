0aca178ce3bee9bf1b62268342937155b475ceb5
Michael Salaverry <michaels@axonize.com>
2019-08-02 18:55:05 +0300

    feat: added tests demonstrating usage

--- __tests__/basics.test.ts
+++ __tests__/basics.test.ts
@@ -28,13 +28,28 @@ describe('Finite State Machine', () => {
     // Assert
     expect(anFSM.getCurrentState()).toEqual(firstState);
   });
+  it('should not allow adding a transition to an unknown state', () => {
+    // Arrange
+    const anFSM = new FSM();
+    // Action
+    anFSM.addState('on');
+    // we're missing the 'off' state
+    const shouldThrow = () => anFSM.on({inputName: 'toggle'}, () => {
+      return new Map([
+        ['on','off'],
+        ['off', 'on'],
+      ])
+    })
+    // Assert
+    expect(shouldThrow).toThrow('unknown state in transition handler')
+  });
   it('should allow transitioning between states', () => {
     // Arrange
     const anFSM = new FSM();
     // Action
     anFSM.addState('on');
     anFSM.addState('off');
-    anFSM.on({inputName: 'toggle'}, function(){
+    anFSM.on({inputName: 'toggle'}, () => {
       return new Map([
         ['on','off'],
         ['off', 'on'],
@@ -52,7 +67,7 @@ describe('Finite State Machine', () => {
     // Action
     fuse.addState('closed');
     fuse.addState('open');
-    fuse.on({inputName: 'powerSurge'}, function(payload){
+    fuse.on({inputName: 'powerSurge'}, (payload) => {
       if (payload > 100) {
         return new Map([
           ['closed', 'open'],
@@ -82,4 +97,102 @@ describe('Finite State Machine', () => {
     // Assert
     expect(fuse.getCurrentState()).toEqual('open');
   });
+  it('should allow multiple FSMs to interact via a mediator', () => {
+    // Arrange
+    const enum TrafficLightColors {
+      'red' = 'red',
+      'yellow' = 'yellow',
+      'green' = 'green'
+    }
+    const TrafficLightFactory = () => {
+      const TrafficLight = new FSM();
+      
+      TrafficLight.addState(TrafficLightColors.red);
+      TrafficLight.addState(TrafficLightColors.yellow);
+      TrafficLight.addState(TrafficLightColors.green);
+      TrafficLight.on({inputName: 'turnExpired'}, () => {
+        return new Map([
+          [TrafficLightColors.green, TrafficLightColors.yellow],
+          [TrafficLightColors.yellow, TrafficLightColors.red],
+          [TrafficLightColors.red, TrafficLightColors.green],
+        ])
+      });
+      return TrafficLight;
+    }
+
+    const NSTrafficLight = TrafficLightFactory();
+    const EWTrafficLight = TrafficLightFactory();
+    // should start the same
+    expect(NSTrafficLight.getCurrentState()).toEqual(TrafficLightColors.red);
+    expect(EWTrafficLight.getCurrentState()).toEqual(TrafficLightColors.red);
+    // changing one shouldn't change the other
+    NSTrafficLight.dispatch({inputName: 'turnExpired'});
+    expect(NSTrafficLight.getCurrentState()).toEqual(TrafficLightColors.green);
+    expect(EWTrafficLight.getCurrentState()).toEqual(TrafficLightColors.red);
+    // let's connect one to the other using a third object
+    enum trafficStates {
+      'trafficMoving' = 'crossTrafficMoving',
+      'allTrafficStopped' = 'allTrafficStopped',
+      'trafficStopping' = 'trafficStopping',
+    }
+    class TrafficLightMediator {
+      private trafficLights: FSM[] = [];
+      private mediatorFSM: FSM = new FSM();
+      constructor(trafficLights: FSM[]) {
+        this.trafficLights = trafficLights;
+        this.mediatorFSM.addState(trafficStates.trafficMoving);
+        this.mediatorFSM.addState(trafficStates.trafficStopping);
+        this.mediatorFSM.addState(trafficStates.allTrafficStopped);
+        this.mediatorFSM.on({inputName: 'tick'}, (payload: TrafficLightColors[] | undefined) => {
+          if (payload) {
+          const isTrafficMoving = () => payload.find(tl => tl === TrafficLightColors.green) !== undefined;
+
+          switch (true) {
+            case isTrafficMoving():
+              const movingTL = this.trafficLights.find(tl => 
+                tl.getCurrentState() === TrafficLightColors.green
+              )
+              if (movingTL) {
+                movingTL.dispatch({inputName: 'turnExpired'})
+              }
+              break;
+            default:
+              this.trafficLights.forEach(tl => tl.dispatch({inputName: 'turnExpired'}))
+              break;
+          }
+        }
+          return new Map([
+            [trafficStates.allTrafficStopped, trafficStates.trafficMoving],
+            [trafficStates.trafficMoving, trafficStates.trafficStopping],
+            [trafficStates.trafficStopping, trafficStates.allTrafficStopped],
+          ])
+        })
+      }
+      turnExpired = () => {
+        const payload = this.trafficLights.map(tl => tl.getCurrentState());
+        this.mediatorFSM.dispatch({inputName: 'tick', payload});
+      }
+    };
+    const TLM = new TrafficLightMediator([NSTrafficLight, EWTrafficLight]);
+    // Action start stopping traffic
+    TLM.turnExpired();
+    // Assert
+    expect(NSTrafficLight.getCurrentState()).toEqual(TrafficLightColors.yellow);
+    expect(EWTrafficLight.getCurrentState()).toEqual(TrafficLightColors.red);
+    // Action allow cross traffic to start
+    TLM.turnExpired();
+    // Assert end of second turn
+    expect(NSTrafficLight.getCurrentState()).toEqual(TrafficLightColors.red);
+    expect(EWTrafficLight.getCurrentState()).toEqual(TrafficLightColors.green);
+    // Action start stopping cross traffic
+    TLM.turnExpired();
+    // Assert end of third turn
+    expect(NSTrafficLight.getCurrentState()).toEqual(TrafficLightColors.red);
+    expect(EWTrafficLight.getCurrentState()).toEqual(TrafficLightColors.yellow);
+    // Action allow traffic to start
+    TLM.turnExpired();
+    // Assert end of fourth turn
+    expect(NSTrafficLight.getCurrentState()).toEqual(TrafficLightColors.green);
+    expect(EWTrafficLight.getCurrentState()).toEqual(TrafficLightColors.red);
+  })
 });
--- src/FSM.ts
+++ src/FSM.ts
@@ -10,28 +10,39 @@ interface TransitionHandler {
 export class FSM {
   private currentState: string = '';
   public states: Map<string, string> = new Map();
-  constructor() {}
   private transitions: Map<string, TransitionHandler> = new Map();
 
-  public addState(state: string): void {
+  public addState = (state: string): void => {
     if (this.currentState === '' && typeof state === 'string') {
       this.currentState = state;
     }
     this.states.set(state, state);
   }
 
-  public on = (input: IInput, reducer: TransitionHandler) => {
-    this.transitions.set(input.inputName, reducer);
+  private checkThatAllTransitionsAreValid = (transitionHandler: TransitionHandler) => {
+    const defaultTransitions = transitionHandler();
+    const possibleStates = [...defaultTransitions.keys()];
+    possibleStates.forEach(state => {
+      if (!this.states.has(state)) {
+        throw new Error('unknown state in transition handler')
+      }
+    });
+  }
+
+  public on = (input: IInput, transitionHandler: TransitionHandler) => {
+    this.checkThatAllTransitionsAreValid(transitionHandler);
+    this.transitions.set(input.inputName, transitionHandler);
   }
 
   private missingTransitionHandlerError = (_?: any) => new Map();
 
   public dispatch = (input: IInput) => {
     const transitionHandler = this.transitions.get(input.inputName) || this.missingTransitionHandlerError;
-    this.currentState = transitionHandler(input.payload).get(this.currentState);
+    const newState = transitionHandler(input.payload).get(this.currentState);
+    this.currentState = newState;
   }
 
-  public getCurrentState(): string {
+  public getCurrentState = (): string => {
     return this.currentState;
   }
 }
