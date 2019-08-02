0e3536674988387a0c2d2721264e06beb6c8a3ec
Michael Salaverry <michaels@axonize.com>
2019-08-02 16:27:20 +0300

    feat: added transitions

--- __tests__/basics.test.ts
+++ __tests__/basics.test.ts
@@ -3,17 +3,83 @@ import { FSM } from '../src/FSM';
 describe('Finite State Machine', () => {
   it('should be initializable with states', () => {
     // Arrange
-    const anFSM = new FSM(['on']);
-
+    const anFSM = new FSM();
+    // Action
+    anFSM.addState('on');
     // Assert
     expect(anFSM).toBeDefined();
-  })
-  it('should have a current state on initialization', () => {
+  });
+  it('should report current state as first state added', () => {
     // Arrange
-    const firstState = 'on'
-    const anFSM = new FSM([firstState]);
-
+    const firstState = 'on';
+    const anFSM = new FSM();
+    // Action
+    anFSM.addState(firstState);
     // Assert
     expect(anFSM.getCurrentState()).toEqual(firstState);
-  })
-})
+  });
+  it('should allow multiple states, and stay the same when adding a state', () => {
+    // Arrange
+    const firstState = 'on';
+    const anFSM = new FSM();
+    // Action
+    anFSM.addState(firstState);
+    anFSM.addState('off');
+    // Assert
+    expect(anFSM.getCurrentState()).toEqual(firstState);
+  });
+  it('should allow transitioning between states', () => {
+    // Arrange
+    const anFSM = new FSM();
+    // Action
+    anFSM.addState('on');
+    anFSM.addState('off');
+    anFSM.on({inputName: 'toggle'}, function(){
+      return new Map([
+        ['on','off'],
+        ['off', 'on'],
+      ])
+    })
+    const { dispatch } = anFSM;
+    // Action
+    dispatch({inputName: 'toggle'});
+    // Assert
+    expect(anFSM.getCurrentState()).toEqual('off');
+  });
+  it('allow states to have attached variables', () => {
+    // Arrange
+    const fuse = new FSM();
+    // Action
+    fuse.addState('closed');
+    fuse.addState('open');
+    fuse.on({inputName: 'powerSurge'}, function(payload){
+      if (payload > 100) {
+        return new Map([
+          ['closed', 'open'],
+          ['open', 'open'],
+        ]);
+      }
+      return new Map([
+        ['closed', 'closed'],
+        ['open', 'open'],
+      ])
+    })
+    const { dispatch } = fuse;
+    // Action
+    dispatch({inputName: 'powerSurge', payload: 99});
+    // Assert
+    expect(fuse.getCurrentState()).toEqual('closed');
+    // Action
+    dispatch({inputName: 'powerSurge', payload: 99});
+    // Assert
+    expect(fuse.getCurrentState()).toEqual('closed');
+    // Action
+    dispatch({inputName: 'powerSurge', payload: 101});
+    // Assert
+    expect(fuse.getCurrentState()).toEqual('open');
+    // Action
+    dispatch({inputName: 'powerSurge', payload: 99});
+    // Assert
+    expect(fuse.getCurrentState()).toEqual('open');
+  });
+});
--- src/FSM.ts
+++ src/FSM.ts
@@ -1,8 +1,34 @@
-export class FSM {
-  private currentState: string;
+interface IInput {
+  inputName: string
+  payload?: any
+}
 
-  constructor(states: string[]) {
-    this.currentState = states[0];
+interface TransitionHandler {
+  (payload?: any): Map<string, string>
+};
+
+export class FSM {
+  private currentState: string = '';
+  public states: Map<string, string> = new Map();
+  constructor() {}
+  private transitions: Map<string, TransitionHandler> = new Map();
+
+  public addState(state: string): void {
+    if (this.currentState === '' && typeof state === 'string') {
+      this.currentState = state;
+    }
+    this.states.set(state, state);
+  }
+
+  public on = (input: IInput, reducer: TransitionHandler) => {
+    this.transitions.set(input.inputName, reducer);
+  }
+
+  private missingTransitionHandlerError = (_?: any) => new Map();
+
+  public dispatch = (input: IInput) => {
+    const transitionHandler = this.transitions.get(input.inputName) || this.missingTransitionHandlerError;
+    this.currentState = transitionHandler(input.payload).get(this.currentState);
   }
 
   public getCurrentState(): string {
